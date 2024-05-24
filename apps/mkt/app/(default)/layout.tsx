a2aa133cdaacccf5deb55509df22ba6836b48fde
Walter Galvao <walterbgneto@gmail.com>
2024-05-23 22:17:30 -0300

    feat(mkt): umami

--- apps/mkt/app/(default)/layout.tsx
+++ apps/mkt/app/(default)/layout.tsx
@@ -2,19 +2,15 @@
 
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
-import dynamic, { Loader } from "next/dynamic";
 
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const CrispChat = dynamic(() => import("../../components/crisp-chat"));
-
   return (
     <>
       <Header />
-      <CrispChat />
 
       <main className="grow">{children}</main>
       <Footer />
--- apps/mkt/app/layout.tsx
+++ apps/mkt/app/layout.tsx
@@ -1,38 +1,53 @@
-import './css/style.css'
+import "./css/style.css";
 
-import { Inter, Inter_Tight } from 'next/font/google'
+import { Inter, Inter_Tight } from "next/font/google";
+import dynamic, { Loader } from "next/dynamic";
+import Script from "next/script";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap'
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
 
 const inter_tight = Inter_Tight({
-  weight: ['500', '600', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-inter-tight',
-  display: 'swap'
-})
+  weight: ["500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  variable: "--font-inter-tight",
+  display: "swap",
+});
 
 export const metadata = {
-  title: 'sweetr.dev',
-  description: 'The platform for productivity and DX',
-}
+  title: "sweetr.dev",
+  description: "The platform for productivity and DX",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const CrispChat = dynamic(() => import("../components/crisp-chat"));
+  const analyticsId = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${inter_tight.variable} font-inter antialiased bg-white text-zinc-900 tracking-tight`}>
+      {analyticsId && (
+        <Script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id={analyticsId}
+        />
+      )}
+      <CrispChat />
+      <body
+        className={`${inter.variable} ${inter_tight.variable} font-inter antialiased bg-white text-zinc-900 tracking-tight`}
+      >
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
         </div>
       </body>
     </html>
-  )
+  );
 }
