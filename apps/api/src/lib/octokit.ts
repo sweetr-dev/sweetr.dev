a4c4d4f736240e8f504419e17e1773d0e008ef1c
Walter Galvao <walterbgneto@gmail.com>
2024-06-29 02:36:01 -0300

    chore: update package-lock

--- apps/api/src/lib/octokit.ts
+++ apps/api/src/lib/octokit.ts
@@ -1,4 +1,4 @@
-import { createAppAuth } from "@octokit/auth-app";
+import { createAppAuth, StrategyOptions } from "@octokit/auth-app";
 import { Octokit } from "octokit";
 import { graphql } from "@octokit/graphql";
 import { config } from "../config";
@@ -17,6 +17,16 @@ export const getAppOctoKit = () =>
     },
   });
 
+const redisCaching: StrategyOptions["cache"] = {
+  async get(key: string) {
+    const cachedValue = await redisConnection.get(`github:token:${key}`);
+    return cachedValue || "";
+  },
+  set(key: string, value: string) {
+    redisConnection.setex(`github:token:${key}`, 3600, value);
+  },
+};
+
 export const getInstallationOctoKit = (installationId: number) =>
   new Octokit({
     authStrategy: createAppAuth,
@@ -24,17 +34,7 @@ export const getInstallationOctoKit = (installationId: number) =>
       appId: config.github.appId,
       privateKey: config.github.privateKey,
       installationId,
-      cache: {
-        async get(key: string) {
-          const cachedValue = await redisConnection.get(
-            `github-rest:token:${key}`
-          );
-          return cachedValue || "";
-        },
-        set(key: string, value: string) {
-          redisConnection.setex(`github-rest:token:${key}`, 3600, value);
-        },
-      },
+      cache: redisCaching,
     },
   });
 
@@ -45,17 +45,7 @@ export const getInstallationGraphQLOctoKit = (installationId: number) =>
         appId: config.github.appId,
         privateKey: config.github.privateKey,
         installationId,
-        cache: {
-          async get(key: string) {
-            const cachedValue = await redisConnection.get(
-              `github-graphql:token:${key}`
-            );
-            return cachedValue || "";
-          },
-          set(key: string, value: string) {
-            redisConnection.setex(`github-graphql:token:${key}`, 3600, value);
-          },
-        },
+        cache: redisCaching,
       }).hook,
     },
   });
