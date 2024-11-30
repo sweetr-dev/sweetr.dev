20ecebb8ae87465c9a38b8fb9b2aef9677a90ba3
Walter Galvao <walterbgneto@gmail.com>
2024-11-30 20:44:45 -0300

    feat(web): link size settings in pr label page

--- apps/web/src/app/automations/settings/pr-size-labeler/components/form-pr-size-labeler-settings/form-pr-size-labeler-settings.tsx
+++ apps/web/src/app/automations/settings/pr-size-labeler/components/form-pr-size-labeler-settings/form-pr-size-labeler-settings.tsx
@@ -1,9 +1,21 @@
-import { ColorInput, Group, Input, Stack, Switch, Title } from "@mantine/core";
+import {
+  ColorInput,
+  Group,
+  Input,
+  Stack,
+  Switch,
+  Title,
+  Text,
+  Divider,
+  Button,
+} from "@mantine/core";
 import { BoxSetting } from "../../../../../../components/box-setting";
 import { UseFormReturnType } from "@mantine/form";
 import { colorPickerSwatches } from "../../../../../../providers/color.provider";
 import { capitalize } from "radash";
 import { FormPrSizeLabeler } from "../../types";
+import { Link } from "react-router-dom";
+import { IconExternalLink } from "@tabler/icons-react";
 
 interface FormPrSizeLabelerSettingsProps {
   form: UseFormReturnType<FormPrSizeLabeler>;
@@ -13,49 +25,68 @@ export const FormPrSizeLabelerSettings = ({
   form,
 }: FormPrSizeLabelerSettingsProps) => {
   return (
-    <Stack p="md">
-      <Title order={5}>Settings</Title>
+    <>
+      <Stack p="md">
+        <Title order={5}>Settings</Title>
 
-      <BoxSetting label="Enabled">
-        <Switch
-          size="lg"
-          color="green.7"
-          onLabel="ON"
-          offLabel="OFF"
-          {...form.getInputProps("enabled", { type: "checkbox" })}
-        />
-      </BoxSetting>
+        <BoxSetting label="Enabled">
+          <Switch
+            size="lg"
+            color="green.7"
+            onLabel="ON"
+            offLabel="OFF"
+            {...form.getInputProps("enabled", { type: "checkbox" })}
+          />
+        </BoxSetting>
 
-      {form.values.enabled &&
-        (
-          Object.keys(form.values.settings.labels) as Array<
-            keyof typeof form.values.settings.labels
-          >
-        ).map((size) => (
-          <BoxSetting
-            key={size}
-            label={capitalize(size)}
-            description={`Customize the "${size}" label`}
-          >
-            <Group gap={5}>
-              <Input
-                maw={140}
-                placeholder={size}
-                {...form.getInputProps(`settings.labels.${size}.label`)}
-                maxLength={100}
-              ></Input>
-              <ColorInput
-                placeholder="#F0F0F0"
-                error={false}
-                swatches={colorPickerSwatches}
-                swatchesPerRow={7}
-                {...form.getInputProps(`settings.labels.${size}.color`)}
-                maw={120}
-                withEyeDropper={false}
-              />
-            </Group>
-          </BoxSetting>
-        ))}
-    </Stack>
+        {form.values.enabled &&
+          (
+            Object.keys(form.values.settings.labels) as Array<
+              keyof typeof form.values.settings.labels
+            >
+          ).map((size) => (
+            <BoxSetting
+              key={size}
+              label={capitalize(size)}
+              description={`Customize the "${size}" label`}
+            >
+              <Group gap={5}>
+                <Input
+                  maw={140}
+                  placeholder={size}
+                  {...form.getInputProps(`settings.labels.${size}.label`)}
+                  maxLength={100}
+                ></Input>
+                <ColorInput
+                  placeholder="#F0F0F0"
+                  error={false}
+                  swatches={colorPickerSwatches}
+                  swatchesPerRow={7}
+                  {...form.getInputProps(`settings.labels.${size}.color`)}
+                  maw={120}
+                  withEyeDropper={false}
+                />
+              </Group>
+            </BoxSetting>
+          ))}
+      </Stack>
+      <Divider my="sm" />
+
+      <Stack p="md">
+        <Title order={5}>Note</Title>
+
+        <Text fz="sm">Looking for Pull Request size customization? </Text>
+        <Button
+          variant="default"
+          component={Link}
+          to="/settings/pull-request"
+          target="_blank"
+          w="fit-content"
+          rightSection={<IconExternalLink stroke={1.5} size={14} />}
+        >
+          Settings
+        </Button>
+      </Stack>
+    </>
   );
 };
