a1c0f05531258c6ca2ed18f07e578c17975e5a4e
Walter Galvao <walterbgneto@gmail.com>
2024-11-30 16:17:14 -0300

    feat(web): support button on size drawer

--- apps/web/src/app/settings/pull-request-settings/size/page.tsx
+++ apps/web/src/app/settings/pull-request-settings/size/page.tsx
@@ -7,16 +7,19 @@ import {
   NumberInput,
   Divider,
   TagsInput,
+  Anchor,
 } from "@mantine/core";
 import { DrawerScrollable } from "../../../../components/drawer-scrollable";
 import { useDrawerPage } from "../../../../providers/drawer-page.provider";
 import { BadgePullRequestSize } from "../../../../components/badge-pull-request-size";
 import { PullRequestSize } from "@sweetr/graphql-types/frontend/graphql";
+import { useSupportChat } from "../../../../components/navbar/use-support-chat";
 
 export const PullRequestSizePage = () => {
   const drawerProps = useDrawerPage({
     closeUrl: `/settings/pull-request`,
   });
+  const { openChat } = useSupportChat();
 
   return (
     <DrawerScrollable
@@ -60,7 +63,18 @@ export const PullRequestSizePage = () => {
           description="Files matching these patterns will be ignored when calculating total changes."
           value={["package-lock.json", "yarn.lock"]}
         />
-        <Text c="dimmed" fz="sm"></Text>
+      </Stack>
+      <Divider my="sm" />
+      <Stack p="md">
+        <Title order={5}>Note</Title>
+        <Text c="dimmed" fz="sm">
+          We recalculate the Pull Request size every time it receives an update.{" "}
+          Reach out to support if you need all past stale Pull Requests
+          recalculated.
+        </Text>
+        <Button onClick={openChat} variant="default" w="fit-content">
+          Chat with support
+        </Button>
       </Stack>
     </DrawerScrollable>
   );
