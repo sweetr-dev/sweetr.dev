00545c481f79430582fdc9e9d1035c663a8a2aef
Walter Galvao <walterbgneto@gmail.com>
2026-02-18 04:12:29 -0300

    fix: code

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -29,10 +29,13 @@ export const logger = {
     const cleanObj = { ...(obj || {}) };
 
     for (const key of Object.keys(cleanObj)) {
-      if (loggableFields[key] && typeof cleanObj[key] === "object" && cleanObj[key] !== null) {
+      if (
+        loggableFields[key] &&
+        typeof cleanObj[key] === "object" &&
+        cleanObj[key] !== null
+      ) {
         cleanObj[key] = pick(cleanObj[key], loggableFields[key]);
       }
-    }
     }
 
     pinoLogger.info(cleanObj, msg);
