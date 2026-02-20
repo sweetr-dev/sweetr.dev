81726948208533ecbb46fc7d4cdf404b9576e767
Walter Galvao <walterbgneto@gmail.com>
2026-02-20 17:06:36 -0300

    fix: add job error data to Sentry (#83)
    * fix: add job error data to Sentry
    
    * fix: fields
    
    * chore: rename field

--- apps/api/src/bull-mq/error-handler.ts
+++ apps/api/src/bull-mq/error-handler.ts
@@ -1,6 +1,7 @@
 import { Job } from "bullmq";
 import { captureException } from "../lib/sentry";
 import { logger } from "../lib/logger";
+import { BaseException } from "../app/errors/exceptions/base.exception";
 
 export const bullMQErrorHandler = (error: Error) => {
   logger.info(`🐂❌ BullMQ: ${error.message}`);
@@ -11,7 +12,15 @@ export const bullMQErrorHandler = (error: Error) => {
 export const workerFailedHandler = (job: Job, error: Error) => {
   logger.info(`🐂❌ BullMQ: ${job.name} - Erroed job #${job.id}`);
 
-  captureException(error, { extra: { jobId: job.id, jobName: job.name } });
+  let errorExtra = {};
+
+  if (error instanceof BaseException) {
+    errorExtra = error.extensions?.extra as Record<string, unknown>;
+  }
+
+  captureException(error, {
+    extra: { jobId: job.id, jobName: job.name, jobData: job.data, errorExtra },
+  });
 };
 
 export const workerStalledHandler = (jobId: string) => {
