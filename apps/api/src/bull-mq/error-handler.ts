2e5ffa52ddf6e805253cb1748f7a6157934f8cd6
Walter Galvao <walterbgneto@gmail.com>
2026-02-20 16:58:09 -0300

    fix: fields

--- apps/api/src/bull-mq/error-handler.ts
+++ apps/api/src/bull-mq/error-handler.ts
@@ -1,6 +1,7 @@
 import { Job } from "bullmq";
 import { captureException } from "../lib/sentry";
 import { logger } from "../lib/logger";
+import { BaseException } from "../app/errors/exceptions/base.exception";
 
 export const bullMQErrorHandler = (error: Error) => {
   logger.info(`🐂❌ BullMQ: ${error.message}`);
@@ -11,8 +12,14 @@ export const bullMQErrorHandler = (error: Error) => {
 export const workerFailedHandler = (job: Job, error: Error) => {
   logger.info(`🐂❌ BullMQ: ${job.name} - Erroed job #${job.id}`);
 
+  let extra = {};
+
+  if (error instanceof BaseException) {
+    extra = error.extensions?.extra as Record<string, unknown>;
+  }
+
   captureException(error, {
-    extra: { jobId: job.id, jobName: job.name, error },
+    extra: { jobId: job.id, jobName: job.name, jobData: job.data, ...extra },
   });
 };
 
