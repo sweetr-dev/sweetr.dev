2cf3b6e76f47dcb9674d6018ab2318f021c0a8c6
Walter Galvao <walterbgneto@gmail.com>
2026-02-20 22:25:01 -0300

    chore: add query logging

--- apps/api/src/prisma.ts
+++ apps/api/src/prisma.ts
@@ -2,7 +2,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { JsonValue } from "@prisma/client/runtime/library";
 import { isObject } from "radash";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ log: ["query"] });
 
 /**
  * Creates a Prisma extension that injects RLS session variables into every
