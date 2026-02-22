a24343e03d7b3ebe492708c75f6b836fb829eded
Walter Galvao <walterbgneto@gmail.com>
2026-02-22 17:33:35 -0300

    feat: upgrade express to 5

--- certs/devcert.js
+++ certs/devcert.js
@@ -1,11 +1,22 @@
 var path = require("path");
 var fs = require("fs");
-var devcert = require("devcert");
+
+try {
+  var devcert = require("devcert");
+} catch {
+  console.error(
+    "Error: 'devcert' is not installed.\n\n" +
+      "Install it globally to generate local SSL certificates:\n\n" +
+      "  npm install -g devcert\n"
+  );
+  process.exit(1);
+}
 
 devcert
   .certificateFor(["app.sweetr.local", "api.sweetr.local"])
   .then(function (certificate) {
     fs.writeFileSync(path.resolve(__dirname, "./tls.key"), certificate.key);
     fs.writeFileSync(path.resolve(__dirname, "./tls.cert"), certificate.cert);
+    console.log("SSL certificates generated successfully.");
   })
   .catch(console.error);
